6c7334fe7a0194583562b358efcb3716bf33136b
sayali1910 <sayalikanase1910@gmail.com>
2023-04-27 23:37:06 +0530

    Fix Nav Link

--- src/components/layout/header/index.tsx
+++ src/components/layout/header/index.tsx
@@ -2,15 +2,16 @@ import React, { useContext, useEffect, useState } from "react";
 import styles from "./index.module.css";
 import { CartDataContext } from "../../../App";
 import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   let { cartData, updateCartData } = useContext(CartDataContext);
   const navigate = useNavigate();
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-info">
-      <a className="navbar-brand" href="#">
+      <Link className="navbar-brand" to="/">
         Zyxware Problem 4
-      </a>
+      </Link>
       <button className={styles.cartButton} onClick={() => navigate("/cart")}>
         🛒
         <span className="badge badge-danger">
